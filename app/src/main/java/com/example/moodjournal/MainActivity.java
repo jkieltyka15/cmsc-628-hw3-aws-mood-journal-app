8e869336685c6e3cfe1b5d47faef2701974e0925
jkieltyka15 <jmkielty@mtu.edu>
2025-05-10 19:40:48 -0400

    MJ: Added skeletons for entry, history and journal fragments.

--- app/src/main/java/com/example/moodjournal/MainActivity.java
+++ app/src/main/java/com/example/moodjournal/MainActivity.java
@@ -2,23 +2,20 @@ package com.example.moodjournal;
 
 import android.os.Bundle;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    /**
+     * Called when Activity is created
+     *
+     * @param savedInstanceState: Most recent data in the save instance state or null
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        // perform standard setup
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
     }
 }
