c80ea3b8839d21c5164abb28427701aba8d43740
jkieltyka15 <jmkielty@mtu.edu>
2025-05-10 18:33:41 -0400

    MJ: Added blank login fragment and added a fragment view container for the main activity.

---
+++ app/src/main/java/com/example/moodjournal/LoginFragment.java
@@ -0,0 +1,31 @@
+package com.example.moodjournal;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class LoginFragment extends Fragment {
+
+    /**
+     * Called when fragment view is created
+     *
+     * @param inflater: Used to inflate any views in the fragment
+     * @param container: Parent view that the fragment UI should be attached to
+     * @param savedInstanceState: Previous saved state
+     *
+     * @return View of fragment
+     */
+    @Override
+    public View onCreateView(LayoutInflater inflater,
+                             ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        // inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
